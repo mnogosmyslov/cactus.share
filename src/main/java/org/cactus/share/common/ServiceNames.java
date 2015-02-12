3670a036e12f79597334af48f9694350c4f9329a
Alexander Mnogosmyslov <mnogosmyslov@hotmail.com>
2015-02-12 23:57:32 +0200

    Fix config files

---
+++ src/main/java/org/cactus/share/common/ServiceNames.java
@@ -0,0 +1,10 @@
+package org.cactus.share.common;
+
+/**
+ * Stores all service names.
+ */
+public abstract class ServiceNames {
+
+    public static final String USER_ACCOUNT_SERVICE = "/UserAccountService";
+
+}
