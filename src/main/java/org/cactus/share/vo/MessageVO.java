82f6b8efe03da78db6c5097d70da907cdad0d4ec
udenfox <udenfox@gmail.com>
2015-04-02 18:11:36 +0300

    Socket now work with pure Websockets too; MessageVO update.

--- src/main/java/org/cactus/share/vo/MessageVO.java
+++ src/main/java/org/cactus/share/vo/MessageVO.java
@@ -1,17 +1,31 @@
 package org.cactus.share.vo;
 
+import java.util.Date;
+
 public class MessageVO {
 
-    private String message;
     private int id;
+    private String message;
+    private int authorId;
+    private Date date;
 
     public MessageVO() {
 
     }
 
-    public MessageVO(int id, String message) {
+    public MessageVO(int id, String message, int authorId, Date date) {
         this.id = id;
         this.message = message;
+        this.authorId = authorId;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getMessage() {
@@ -22,11 +36,19 @@ public class MessageVO {
         this.message = message;
     }
 
-    public int getId() {
-        return id;
+    public int getAuthorId() {
+        return authorId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
